c5da2bfe00836e1fdd1ea800023581869379e787
Joseph Preimesberger <j.preimesberger@gmail.com>
2024-01-02 18:12:21 -0600

    refactored question/answer logic to make more efficent

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,99 +25,109 @@ var timeLeft = 0;
 var timeInterval;
 
 var questionArray = [];
-var choicesArray = [];
-var question1 = "Commonly used data types DO NOT include:";
-var question2 =
-  "The condition in an if/else statement is enclosed within _____.";
-var question3 = "Arrays in JavaScript can be used to store _____.";
-var question4 =
-  "String values must be enclosed within _____ when being assigned to variables.";
-var question5 =
-  "A very useful tool used during development and debugging for printing content to the debugger is:";
+
+var question1 = {
+  text: "Commonly used data types DO NOT include:",
+  answer1: {
+    text: "strings",
+    correct: false,
+  },
+  answer2: {
+    text: "booleans",
+    correct: false,
+  },
+  answer3: {
+    text: "alerts",
+    correct: true,
+  },
+  answer4: {
+    text: "numbers",
+    correct: false,
+  },
+  answered: false,
+};
+var question2 = {
+  text: "The condition in an if/else statement is enclosed within _____.",
+  answer1: {
+    text: "quotes",
+    correct: false,
+  },
+  answer2: {
+    text: "curly brackets",
+    correct: true,
+  },
+  answer3: {
+    text: "parentheses",
+    correct: false,
+  },
+  answer4: {
+    text: "square brackets",
+    correct: false,
+  },
+  answered: false,
+};
+var question3 = {
+  text: "Arrays in JavaScript can be used to store _____.",
+  answer1: {
+    text: "numbers and strings",
+    correct: false,
+  },
+  answer2: {
+    text: "other arrays",
+    correct: false,
+  },
+  answer3: {
+    text: "booleans",
+    correct: false,
+  },
+  answer4: {
+    text: "all of the above",
+    correct: true,
+  },
+  answered: false,
+};
+var question4 = {
+  text: "String values must be enclosed within _____ when being assigned to variables.",
+  answer1: {
+    text: "commas",
+    correct: false,
+  },
+  answer2: {
+    text: "curly brackets",
+    correct: false,
+  },
+  answer3: {
+    text: "quotes",
+    correct: true,
+  },
+  answer4: {
+    text: "parentheses",
+    correct: false,
+  },
+  answered: false,
+};
+var question5 = {
+  text: "A very useful tool used during development and debugging for printing content to the debugger is:",
+  answer1: {
+    text: "JavaScript",
+    correct: false,
+  },
+  answer2: {
+    text: "terminal/bash",
+    correct: false,
+  },
+  answer3: {
+    text: "for loops",
+    correct: false,
+  },
+  answer4: {
+    text: "console.log",
+    correct: true,
+  },
+  answered: false,
+};
 
 questionArray.push(question1, question2, question3, question4, question5);
-
-var choices1 = {
-  text1: "strings",
-  correct1: false,
-  text2: "booleans",
-  correct2: false,
-  text3: "alerts",
-  correct3: true,
-  text4: "numbers",
-  correct4: false,
-};
-var choices2 = {
-  text1: "quotes",
-  correct1: false,
-  text2: "curly brackets",
-  correct2: true,
-  text3: "parentheses",
-  correct3: false,
-  text4: "square brackets",
-  correct4: false,
-};
-var choices3 = {
-  text1: "numbers and strings",
-  correct1: false,
-  text2: "other arrays",
-  correct2: false,
-  text3: "booleans",
-  correct3: false,
-  text4: "all of the above",
-  correct4: true,
-};
-var choices4 = {
-  text1: "commas",
-  correct1: false,
-  text2: "curly brackets",
-  correct2: false,
-  text3: "quotes",
-  correct3: true,
-  text4: "parentheses",
-  correct4: false,
-};
-var choices5 = {
-  text1: "JavaScript",
-  correct1: false,
-  text2: "terminal/bash",
-  correct2: false,
-  text3: "for loops",
-  correct3: false,
-  text4: "console.log",
-  correct4: true,
-};
-
-choicesArray.push(choices1, choices2, choices3, choices4, choices5);
-
-var answer1 = {
-  name: "answer1",
-  number: "1. ",
-  text: choicesArray[0].text1,
-  correct: choicesArray[0].correct1,
-};
-var answer2 = {
-  name: "answer2",
-  number: "2. ",
-  text: choicesArray[0].text2,
-  correct: choicesArray[0].correct2,
-};
-var answer3 = {
-  name: "answer3",
-  number: "3. ",
-  text: choicesArray[0].text3,
-  correct: choicesArray[0].correct3,
-};
-var answer4 = {
-  name: "answer4",
-  number: "4. ",
-  text: choicesArray[0].text4,
-  correct: choicesArray[0].correct4,
-};
-var answerArray = [answer1, answer2, answer3, answer4];
-var answerNameArray = [answer1.name, answer2.name, answer3.name, answer4.name];
-
-var chosenAnswer;
 
 var score = 0;
 var finalScore = 0;
@@ -130,71 +140,76 @@ startButton.addEventListener("click", function (event) {
   quizFooter.setAttribute("style", "border-top: 2px solid gray;");
   page = "quiz";
   clearInterval(timeInterval);
-  nextQuestion();
+  renderQuestion();
   countdown();
 });
 
-viewScoresButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  if (page !== "quiz") {
-    viewScores();
-  }
-});
+function renderQuestion() {
+  var choice1 = {
+    text: "1. " + questionArray[0].answer1.text,
+    correct: questionArray[0].answer1.correct,
+  };
+  var choice2 = {
+    text: "2. " + questionArray[0].answer2.text,
+    correct: questionArray[0].answer2.correct,
+  };
+  var choice3 = {
+    text: "3. " + questionArray[0].answer3.text,
+    correct: questionArray[0].answer3.correct,
+  };
+  var choice4 = {
+    text: "4. " + questionArray[0].answer4.text,
+    correct: questionArray[0].answer4.correct,
+  };
+
+  quizHeader.textContent = questionArray[0].text;
+  answerButtons[0].textContent = choice1.text;
+  answerButtons[0].setAttribute("correct", choice1.correct);
+  answerButtons[1].textContent = choice2.text;
+  answerButtons[1].setAttribute("correct", choice2.correct);
+  answerButtons[2].textContent = choice3.text;
+  answerButtons[2].setAttribute("correct", choice3.correct);
+  answerButtons[3].textContent = choice4.text;
+  answerButtons[3].setAttribute("correct", choice4.correct);
+}
 
 answerButtons.forEach(function (answer) {
   answer.addEventListener("click", function (event) {
-    chosenAnswer = event.target.getAttribute("class").toString();
-    evaluateAnswer();
+    evaluateAnswer(event.target);
   });
 });
 
-function nextQuestion() {
-  quizHeader.textContent = questionArray[0];
-  answerArray[0].text = choicesArray[0].text1;
-  answerArray[0].correct = choicesArray[0].correct1;
-  answerArray[1].text = choicesArray[0].text2;
-  answerArray[1].correct = choicesArray[0].correct2;
-  answerArray[2].text = choicesArray[0].text3;
-  answerArray[2].correct = choicesArray[0].correct3;
-  answerArray[3].text = choicesArray[0].text4;
-  answerArray[3].correct = choicesArray[0].correct4;
-  answerButtons[0].textContent = answerArray[0].number + answerArray[0].text;
-  answerButtons[1].textContent = answerArray[1].number + answerArray[1].text;
-  answerButtons[2].textContent = answerArray[2].number + answerArray[2].text;
-  answerButtons[3].textContent = answerArray[3].number + answerArray[3].text;
-}
-
-function evaluateAnswer() {
-  var chosenAnswerIndex = answerNameArray.indexOf(chosenAnswer);
-
-  questionArray.shift();
-  choicesArray.shift();
-
-  if (answerArray[chosenAnswerIndex].correct) {
+function evaluateAnswer(chosenAnswer) {
+  if (chosenAnswer.getAttribute("correct") === "true") {
     score += 10;
-    quizFooter.textContent = "Correct!";
+    quizFooter.textContent = "Correct! +10 points";
   } else {
     timeLeft = timeLeft - 10;
     quizFooter.textContent = "Wrong!";
   }
 
-  if (questionArray.length === 0) {
-    finalScore = score + timeLeft;
-    clearInterval(timeInterval);
-    enterInitials(finalScore);
-    quizFooter.textContent = "";
-  } else {
-    nextQuestion();
-  }
+  questionArray.shift();
+
+  nextQuestion();
 }
 
-function enterInitials() {
+function nextQuestion() {
+if (questionArray.length > 0) {
+  renderQuestion();
+}
+else {
+  finalScore = score + timeLeft;
+  clearInterval(timeInterval);
+  enterInitials(finalScore);
+}
+}
+
+function enterInitials(finalScore) {
   hideQuiz();
   page = "enter";
   enterScoreHeader.setAttribute("style", "display:block;");
   enterScoreContent.setAttribute("style", "display:block;");
   finalScoreMsg.textContent = "Your final score is " + finalScore;
-  clearInterval(timeInterval);
 }
 
 var scoreEntry;
@@ -216,6 +231,13 @@ submitScoreButton.addEventListener("click", function (event) {
   scoreEntries.push(scoreEntry);
   localStorage.setItem("scoreEntries", JSON.stringify(scoreEntries));
   viewScores();
+});
+
+viewScoresButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (page !== "quiz") {
+    viewScores();
+  }
 });
 
 function viewScores() {
@@ -253,9 +275,9 @@ backButton.addEventListener("click", function (event) {
 });
 
 clearScoresButton.addEventListener("click", function (event) {
-    localStorage.clear();
-    highscoresList.innerHTML = "";
-})
+  localStorage.clear();
+  highscoresList.innerHTML = "";
+});
 
 function countdown() {
   timeLeft = 75;
